a796caea6c7b18817982777ed5dbacb2f2d48ea4
Christian Ceciliano <cristiancehi@gmail.com>
2014-06-16 02:12:40 -0500

    Algunos cambios y pruebas.

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ var http  = require('http'),
     io    = require('socket.io')(server);
 
 
-var viewsCache = {less: {}, jade: {}};
+var viewsCache = {less: {}, jade: {}, js: {}};
 var renderCSS = function(archivo, callback) {
   if (!viewsCache.less[archivo]) {
     fs.readFile('./less/'+archivo+'.less', {encoding: 'UTF-8', flag: 'r'}, function(err, data){
@@ -42,6 +42,20 @@ var renderHTML = function(archivo, callback) {
     callback(null, viewsCache.jade[archivo]);
   }
 }
+var renderJS = function(archivo, callback) {
+  if (!viewsCache.jade[archivo]) {
+    fs.readFile('./js/'+archivo+'.js', {encoding: 'UTF-8', flag: 'r'}, function(err, data){
+      if (err) {
+        callback(err, null);
+        return;
+      }
+      viewsCache.jade[archivo] = data;
+      callback(null, viewsCache.jade[archivo]);
+    });
+  } else {
+    callback(null, viewsCache.jade[archivo]);
+  }
+}
 
 var MongoClient = require('mongodb').MongoClient;
 var MongoServer = require('mongodb').Server;
@@ -60,6 +74,18 @@ mongoclient.open(function (e, client) {
   });
   app.get('/static/css/:archivo.css', function(req, res, next){
     renderCSS(req.params.archivo, function(err, css){
+      if (!err) {
+        res.set('Content-Type', 'text/css')
+        res.send(200, css);
+      }else{
+        console.error(err);
+        next();
+        return;
+      }
+    });
+  });
+  app.get('/static/js/:archivo.js', function(req, res, next){
+    renderJS(req.params.archivo, function(err, css){
       if (!err) {
         res.set('Content-Type', 'text/css')
         res.send(200, css);
